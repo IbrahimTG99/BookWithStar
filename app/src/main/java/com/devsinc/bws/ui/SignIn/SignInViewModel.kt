5cf173e615f98762789f8e341b463a06100fccd9
IbrahimTariqDev <ibrahim.tariq@devsinc.com>
2022-12-15 23:08:07 +0500

    Setup Ui for SignIn Setup for SignUp (wip) added gradient color to button

---
+++ app/src/main/java/com/devsinc/bws/ui/SignIn/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package com.devsinc.bws.ui.SignIn
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/devsinc/bws/ui/SignUp/SignUpViewModel.kt
@@ -0,0 +1,7 @@
+package com.devsinc.bws.ui.SignUp
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
